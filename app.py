706cc0244643ef18aba0b1557d66a12bc5472384
Techiral <115097129+Techiral@users.noreply.github.com>
2022-10-16 05:55:52 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+# Install required packages
+#pip install flask
+
+#Or
+
+#pip3 install -r requirements.txt
+
+#import required libraries
+from flask import Flask
+
+app = Flask(__name__)
+
+#app routes
+@app.route('/')
+def home():
+    return "Welcome To Flask Tutorial"
+
+@app.route('/dash')
+def dash():
+    return "See the dashboard"
+
+@app.route('/form')
+def form():
+    return "Give feedback"
